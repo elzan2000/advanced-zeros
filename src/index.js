52f2a2c179a992beb9aa8677749c5637cd0b4bb1
Andrei Aksiantsiuk <frith2000@gmail.com>
2018-03-01 20:36:41 +0300

    elzan solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var zeroes = number;
+  var b = base;
+  for (var i = 2; i <= base; i++) {
+    if (b % i === 0) {
+      var k = 0;
+      while (b % i === 0) {
+         b = b / i;     
+         k++;
+      }
+      var c = 0;
+      var z = Math.floor(number / i);
+      while (z > 0) {
+        c += z;
+        z = Math.floor(z / i);
+      }
+      zeroes = Math.min(zeroes, Math.floor(c / k))
+    }
+  }
+  return zeroes;
 }
